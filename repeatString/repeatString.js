da503e96920e731d3d52a05138228cdafe8f8538
Johnathan James <jmjames673@gmail.com>
2022-09-05 20:53:19 -0400

    Solve first test with for loop to concat string argument with one another based on n count

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
+const repeatString = function(str, n) {
+  
+  let res = '';
 
+  for (let i = 0; i < n; i++) {
+    res += str;
+  }
+
+  return res;
 };
+
+console.log(repeatString('ya',3));
 
 module.exports = repeatString;
