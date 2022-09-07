8577c39e9510678355499ab49408752057d0bc4c
Johnathan James <jmjames673@gmail.com>
2022-09-06 20:19:37 -0400

    Solve first test

--- reverseString/reverseString.js
+++ reverseString/reverseString.js
@@ -1,5 +1,17 @@
-const reverseString = function() {
+const reverseString = function(str) {
+  
+  let stringArray = str.split('');
+  let reverseString = "";
+  let n = stringArray.length;
 
+  for (let i = 0; i < n; i++) {
+    reverseString += stringArray.pop();
+  } 
+  
+  return reverseString;
 };
+
+// let testArray = reverseString('spring');
+// console.log(testArray);
 
 module.exports = reverseString;
