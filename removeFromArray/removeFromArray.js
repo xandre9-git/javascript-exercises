b4b8202784cd6aa8ba71d306acb9d8bc7f621fde
Johnathan James <jmjames673@gmail.com>
2022-09-07 17:13:03 -0400

    Solve all tests with nested for loops to iterate array and arguments

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,26 +1,16 @@
-const removeFromArray = function(arr, item) {
-
-  // iterate through array
-  // search for item in current iteration
-  // if found, note index location of item
-  // remove item from array
-  // continue iteration for remaining items in array
-
+const removeFromArray = function (arr, ...items) {
   let length = arr.length;
 
   for (let i = 0; i < length; i++) {
-    if (arr[i] === item) {
-      console.log('Found it!');
-      arr.splice(i,1);
+    for (let j = 0; j < items.length; j++) {
+      if (arr[i] === items[j]) {
+        arr.splice(i, 1);
+        i = 0;
+      }
     }
   }
 
-return arr;
-
+  return arr;
 };
-
-// const arrayOfAnimals = ['otter', 'rabbit', 'human'];
-
-// removeFromArray(arrayOfAnimals, 'otter');
 
 module.exports = removeFromArray;
