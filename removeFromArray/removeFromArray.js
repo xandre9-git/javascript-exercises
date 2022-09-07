3c2b330513fe2996935124d2e8b0a0a6742f37cb
Johnathan James <jmjames673@gmail.com>
2022-09-07 16:41:05 -0400

    Solve first test

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,5 +1,26 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, item) {
+
+  // iterate through array
+  // search for item in current iteration
+  // if found, note index location of item
+  // remove item from array
+  // continue iteration for remaining items in array
+
+  let length = arr.length;
+
+  for (let i = 0; i < length; i++) {
+    if (arr[i] === item) {
+      console.log('Found it!');
+      arr.splice(i,1);
+    }
+  }
+
+return arr;
 
 };
+
+// const arrayOfAnimals = ['otter', 'rabbit', 'human'];
+
+// removeFromArray(arrayOfAnimals, 'otter');
 
 module.exports = removeFromArray;
