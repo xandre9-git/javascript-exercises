df72598761df4a7d0ce14bbc4d7b093bb5319954
Johnathan James <jmjames673@gmail.com>
2022-09-09 19:59:04 -0400

    Solve all tests

--- tempConversion/tempConversion.js
+++ tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-const ftoc = function() {
-
+const ftoc = function(farenheit) {
+  let celsius = (farenheit - 32) * (5 / 9);
+  return Number(celsius.toFixed(1));
 };
 
-const ctof = function() {
-
+const ctof = function(celsius) {
+  let farenheit = (celsius * 9/5) + 32
+  return Number(farenheit.toFixed(1));
 };
 
 module.exports = {
