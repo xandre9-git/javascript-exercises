717d83f6ac198c6ac16682a66cffdd7b08c3cd39
Johnathan James <jmjames673@gmail.com>
2022-09-13 20:48:02 -0400

    Refactor code

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,31 +1,21 @@
 const sumAll = function (start, range) {
-
   let sum = 0;
 
-  // console.log(start);
-  // console.log(range);
-
-  if ((start < 0 )|| (range < 0)) {
+  if (
+    start < 0 ||
+    range < 0 ||
+    isNaN(start) ||
+    isNaN(range) ||
+    typeof start !== "number" ||
+    typeof range !== "number"
+  ) {
     return sum = "ERROR";
   }
 
-  if (isNaN(start) || isNaN(range)) {
-    return sum = "ERROR";
-  }
-
-  if (typeof start !== "number" || typeof range !== "number") {
-    return sum = "ERROR";
-  }
-  
   for (let i = 0; i <= (range > start ? range : start); i++) {
-    // sum += parseInt(range[i], 10);
     sum += i;
-    // console.log(sum);
   }
   return sum;
 };
-
-// console.log(sumAll(123, 1)); // 7626
-
 
 module.exports = sumAll;
