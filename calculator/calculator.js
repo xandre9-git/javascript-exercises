fd196dc58897e8e244cfc11dc495ff1d2dbb42da
Johnathan James <jmjames673@gmail.com>
2022-09-14 19:11:44 -0400

    Solve all tests

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,25 +1,40 @@
-const add = function() {
-	
+const add = function(x,y) {
+	return parseInt(x) + parseInt(y);
 };
 
-const subtract = function() {
-	
+const subtract = function(x,y) {
+	return parseInt(x) - parseInt(y);
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  return arr.reduce((acc,curr) => acc + curr,0)	
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((acc, curr) => acc * curr);
 };
 
-const power = function() {
-	
+const power = function(base,exp) {
+  res = base;
+  for (i = 2; i <= exp; i++){
+    res = res * base;    
+  }
+	return res;
 };
 
-const factorial = function() {
-	
+
+const factorial = function(n) {
+  if (n == 0 || n == 1) {
+    return n = 1;
+  } if (n == 2) {
+    return n = 2;
+  } else {
+    let factorial = []
+    for (i=1;i<=n;i++){
+      factorial.push(i);
+    }
+    return multiply(factorial)
+  }
 };
 
 module.exports = {
