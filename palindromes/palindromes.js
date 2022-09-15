ae834119568da71ff4ce91da78ae64700ee622fa
Johnathan James <jmjames673@gmail.com>
2022-09-14 20:42:45 -0400

    Solve all tests

--- palindromes/palindromes.js
+++ palindromes/palindromes.js
@@ -1,5 +1,25 @@
-const palindromes = function () {};
-
+const palindromes = function (str) {
+  // remove punctuation marks from string using .replace and regex /[.,\/#!$%\^&\*;:{}=\-_`~()]/
+  str = str.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()\s]/g, "");
+  // set all characters of string to lowercase to ease comparisons
+  str = str.toLowerCase();
+  // split() to separate each character of string and insert into an array as separate items
+  const arr = str.split("");
+  // create for loop to iterate through newly created array
+  for (let i = 0; i < arr.length; i++) {
+    // on each iteration check that first item in array is not undefined and that the first item matches the last item
+    if (arr[0] !== undefined && arr[0] == arr[arr.length - 1]) {
+      // delete first item and second item
+      arr.shift();
+      arr.pop();
+    }
+  }
+  // after loop, if first item is same as last item, return true
+  if (arr[0] === arr[arr.length - 1]) {
+    return true;
+  } else {
+    return false;
+  }
 };
 
 module.exports = palindromes;
