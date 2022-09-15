57e5c983eb0241829e57e2cbacf40441bfd7215b
Johnathan James <jmjames673@gmail.com>
2022-09-15 18:46:18 -0400

    Solve all tests

--- getTheTitles/getTheTitles.js
+++ getTheTitles/getTheTitles.js
@@ -1,5 +1,14 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(arr) {
+  // create an empty array to hold book titles
+  const bookTitles = [];
+  // for loop to iterate all items in array of books and authors
+  for (i = 0; i < arr.length; i++){
+    // push only titles into a new array with each iteration
+    bookTitles.push(arr[i]["title"]);
+  }
+  // return new array
+  return bookTitles;
 };
+
 
 module.exports = getTheTitles;
